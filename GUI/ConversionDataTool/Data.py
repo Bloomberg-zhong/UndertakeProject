e4549d6cbb172ebd3bbb96ef6e6507de574a9edf
zhonon <z136303452@hotmail.com>
2020-06-18 18:59:53 +0800

    完成主逻辑，进行实测。

--- GUI/ConversionDataTool/Data.py
+++ GUI/ConversionDataTool/Data.py
@@ -12,23 +12,26 @@ from GUI.ConversionDataTool.utils import *
 import sys
 from os import path
 
+CV_VALUES_TYPE = [
+    '报关费', '港建费', '商检解锁费', '快检费', '续页费',
+    '续柜费', '查柜费', '改船名费', '熏蒸费', '过磅费',
+    '代理费', '其他费用'
+]
 class ReportGeneraler():
     def __init__(self,
                  input_file_path,
-                 output_file_path,
                  _debug = True
                  ):
 
         self.input_file_path = input_file_path
+        self._debug =_debug
+
+    def file_path_check(self,output_file_path):
         self.output_file_path = output_file_path
         self.mailsendlog_path = path.join(output_file_path,u'邮件发送日期日志')
         self.dailylog_path = path.join(output_file_path ,u'日数据')
         self.weeklylog_path = path.join(output_file_path ,u'周数据')
         self.monthlog_path = path.join(output_file_path ,u'月数据')
-        self._debug =_debug
-
-    @staticmethod
-    def file_path_check(self):
         mkdir(self.mailsendlog_path)
         mkdir(self.dailylog_path)
         mkdir(self.weeklylog_path)
@@ -40,7 +43,7 @@ class ReportGeneraler():
         try:
             origin_df = pd.read_excel(self.input_file_path, sheet_name=1)
             users_df = pd.read_excel(self.input_file_path, sheet_name=0)
-        except ValueError:
+        except :
             return '文件无法打开，请检查!'
 
         ORDERS_COLUMNS_LIST=[
@@ -61,7 +64,7 @@ class ReportGeneraler():
         # 订单时间格式检查
         try:
             origin_df['订单时间'] = pd.to_datetime(origin_df['订单时间'])
-        except ValueError:
+        except :
             return '请检查 订单时间 列数据格式,标准格式为 2020-06-01 '
 
         # 费用数据格式检查
@@ -72,10 +75,13 @@ class ReportGeneraler():
                 '代理费', '其他费用', '合计', '返佣费',
             ]
             origin_df[VALUESTYPE] = origin_df[VALUESTYPE].astype('float')
-        except ValueError:
+        except :
             return '请检查 各项费用 列数据格式,默认应该是数字类型'
         if self._debug :
             print('All Check Finish!!')
+
+        return True
+
 
 
     def input_data_check(self):
@@ -87,7 +93,7 @@ class ReportGeneraler():
             not_excent_users_str = ",".join(not_excent_users)
             if not_excent_users_str != '':
                 return '以下客户在客户信息表中不存在信息:{}'.format(not_excent_users_str)
-        except ValueError:
+        except :
             return '请检查客户基本信息表中 客户名称 与订单流水表中 客户名称 是否存在'
         # 邮箱检查
         try:
@@ -95,7 +101,7 @@ class ReportGeneraler():
             empty_email_add_str = ",".join(empty_email_add)
             if empty_email_add_str != '':
                 return '以下客户在客户信息表中不存邮箱地址:{}'.format(empty_email_add_str)
-        except ValueError:
+        except :
             return '客户信息表中邮箱地址至少存在一条才能进行发送'
 
         # 客户表邮箱发送规则
@@ -112,36 +118,75 @@ class ReportGeneraler():
             empty_send_mail_type_str = ",".join(all_empty_send_mail_type_list)
             if empty_send_mail_type_str != '':
                 return '以下客户在客户信息表中不存是否发送邮件规则:{}'.format(empty_send_mail_type_str)
-        except ValueError:
+        except :
             return '客户信息表中必须包含发送规则，具体发送那种规则数据，且必须为\'是|否\''
         if self._debug :
             print('All Check Finish!!')
+        return True
 
 
     def file_data_adj(self):
         self.input_file_check()
-        self.input_data_check()
+
         users_df = pd.read_excel(self.input_file_path, sheet_name=0)
         origin_df = pd.read_excel(self.input_file_path, sheet_name=1)
-        CV_VALUES_TYPE = [
-            '报关费', '港建费', '商检解锁费', '快检费', '续页费',
-            '续柜费', '查柜费', '改船名费', '熏蒸费', '过磅费',
-            '代理费', '其他费用', '返佣费',
-        ]
         origin_df[CV_VALUES_TYPE] = origin_df[CV_VALUES_TYPE].astype(float)
-        origin_df.pivot_table(index=['订单时间', '客户名称', '经营单位'], values=CV_VALUES_TYPE)
+        self.finis_adj_input_df = origin_df
+        return origin_df
 
 
 
+    def file_data_excel_output(self,output_type=0):
+        try:
+            input_df_adj = self.file_data_adj()
+            users_name_list = input_df_adj['客户名称'].unique().tolist()
+            # 按天的方式进行输出数据
+            if output_type == 0:
+                input_df_adj['归属日']= input_df_adj['订单时间'].astype('str')
+                datetime_list =input_df_adj['归属日'].unique().tolist()
+                temp_path=self.dailylog_path
+            # 按照周的方式进行输出
+            elif output_type == 1:
+                 input_df_adj['归属周']= input_df_adj['订单时间'].apply(lambda x: get_week_and_month(date_str=x,type=1)).astype('str')
+                 datetime_list =input_df_adj['归属周'].unique().tolist()
+                 temp_path = self.weeklylog_path
 
+            # 按照月的方式进行输出
+            elif output_type == 2:
+                input_df_adj['归属月']= input_df_adj['订单时间'].apply(lambda x: get_week_and_month(date_str=x,type=0)).astype('str')
+                datetime_list= input_df_adj['归属月'].unique().tolist()
+                temp_path = self.monthlog_path
+            else:
+                raise ('output_type should in (0,1,2)')
 
-    def file_data_word_output(self):
-        pass
+            for i in range(len(datetime_list)):
+                _temp_file_path = path.join(temp_path, datetime_list[i])
+                mkdir(_temp_file_path)
+                for j in range(len(users_name_list)):
+                    if output_type == 0:
+                        _temp_file_adj = input_df_adj[(input_df_adj['归属日'] == datetime_list[i]) & (
+                                input_df_adj['客户名称'] == users_name_list[j])].copy().reset_index(drop=True)
 
-    def file_data_excel_output(self):
-        pass
+                    elif output_type == 1:
+                        _temp_file_adj = input_df_adj[(input_df_adj['归属周'] == datetime_list[i]) & (
+                                input_df_adj['客户名称'] == users_name_list[j])].copy().reset_index(drop=True)
+                    else:
+                        _temp_file_adj = input_df_adj[(input_df_adj['归属月'] == datetime_list[i]) & (
+                                input_df_adj['客户名称'] == users_name_list[j])].copy().reset_index(drop=True)
+                    if _temp_file_adj.empty:
+                        continue
+                    _temp_file_adj['合计'] = _temp_file_adj[CV_VALUES_TYPE].apply(lambda x: x.sum(), axis=1)
+                    _temp_file_adj.loc['合计'] = _temp_file_adj[CV_VALUES_TYPE + ['合计']].apply(lambda x: x.sum())
+                    _temp_file_name = datetime_list[i] + '客户--' + users_name_list[j] + '.xlsx'
+                    _temp_file_adj.to_excel(path.join(_temp_file_path, _temp_file_name), index=True)
+        except:
+            return '发生未知错误'
+
+        return True
+
 
     def send_file_table_outpu(self):
+
         pass
 if __name__ == '__main__':
 
--- GUI/ConversionDataTool/main.py
+++ GUI/ConversionDataTool/main.py
@@ -18,7 +18,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from GUI.ConversionDataTool.MainWindow import Ui_MainWindow
 from GUI.ConversionDataTool.GeneralSender import Sender_Dialog
-
+from GUI.ConversionDataTool.Data import ReportGeneraler
 class MainWindow(QMainWindow, Ui_MainWindow,):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -28,10 +28,15 @@ class MainWindow(QMainWindow, Ui_MainWindow,):
         self.setupUi(self)
         # 打开文件功能
         # self.init_output_filepath
+        self.init_output_filepath()
         self.ChooseFileButon.clicked.connect(self.openfile)
         self.ChooseFileButon.clicked.connect(self.creat_table_show)
         self.FreshDataButton.clicked.connect(self.refresh_display)
         self.OutputFilsePathButton.clicked.connect(self.save_output_filepath)
+        self.DailyDataButton.clicked.connect(self.save_output_daily_file)
+        self.WeeklyDataButton.clicked.connect(self.save_output_weekly_file)
+        self.MonthDataButton.clicked.connect(self.save_output_month_file)
+
 
     def init_output_filepath(self):
         # 初始化登录信息
@@ -47,10 +52,10 @@ class MainWindow(QMainWindow, Ui_MainWindow,):
         if hasattr(self,'path_openfile_name'):
             if len(path_openfile_name) > 0:
                 print(self.path_openfile_name)
-                self.ExcelFilesState.setText('{:<10}'.format(self.path_openfile_name))
+                self.ExcelFilesState.setText('{:<40}'.format(self.path_openfile_name))
         else:
             print('Not choose File!')
-            self.ExcelFilesState.setText('{:<10}'.format(u'没有选择文件'))
+            self.ExcelFilesState.setText('{:<40}'.format(u'没有选择文件'))
             self.openfile()
             self.creat_table_show()
 
@@ -68,6 +73,56 @@ class MainWindow(QMainWindow, Ui_MainWindow,):
         settings.setValue("password", self.lineEdit_password.text())
         settings.setValue("remeberpassword", self.checkBox_remeberpassword.isChecked())
         settings.setValue("autologin", self.checkBox_autologin.isChecked())
+
+    def save_output_daily_file(self,):
+        self.init_output_filepath()
+        if hasattr(self,'path_openfile_name'):
+            RG = ReportGeneraler(input_file_path=self.path_openfile_name)
+            RG.file_path_check(output_file_path = self.output_path)
+            file_check = RG.input_file_check()
+            if file_check is not True:
+                self.ExcelFilesState.setText('{:<40}'.format(file_check))
+            File_data = RG.input_data_check()
+            if File_data is not True :
+                self.ExcelFilesState.setText('{:<40}'.format(File_data))
+            file_save_state = RG.file_data_excel_output(output_type=0)
+            if file_save_state is not True:
+                self.ExcelFilesState.setText('{:<40}'.format(file_save_state))
+        else:
+            self.ExcelFilesState.setText('{:<40}'.format('请选择文件'))
+
+    def save_output_weekly_file(self,):
+        if hasattr(self,'path_openfile_name'):
+            RG = ReportGeneraler(input_file_path=self.path_openfile_name)
+            RG.file_path_check(output_file_path = self.output_path)
+            file_check = RG.input_file_check()
+            if file_check is not True:
+                self.ExcelFilesState.setText('{:<40}'.format(file_check))
+            File_data = RG.input_data_check()
+            if File_data is not True:
+                self.ExcelFilesState.setText('{:<40}'.format(File_data))
+            file_save_state=RG.file_data_excel_output(output_type=1)
+            if file_save_state is not True:
+                self.ExcelFilesState.setText('{:<40}'.format(file_save_state))
+        else:
+            self.ExcelFilesState.setText('{:<40}'.format('请选择文件'))
+
+    def save_output_month_file(self,):
+        if hasattr(self,'path_openfile_name'):
+            RG = ReportGeneraler(input_file_path=self.path_openfile_name)
+            RG.file_path_check(output_file_path = self.output_path)
+            file_check = RG.input_file_check()
+            if file_check is not True :
+                self.ExcelFilesState.setText('{:<40}'.format(file_check))
+            File_data = RG.input_data_check()
+            if File_data is not True:
+                self.ExcelFilesState.setText('{:<40}'.format(File_data))
+            file_save_state=RG.file_data_excel_output(output_type=2)
+            if file_save_state is not True :
+                self.ExcelFilesState.setText('{:<40}'.format(file_save_state))
+        else:
+            self.ExcelFilesState.setText('{:<40}'.format('请选择文件'))
+
 
     def openfile(self):
 
@@ -87,7 +142,18 @@ class MainWindow(QMainWindow, Ui_MainWindow,):
         self.ExcelFilesState.setText('{:<40}'.format(path_openfile_name))
         ###===========读取表格，转换表格，===========================================
         if len(path_openfile_name) > 0:
-            input_table = pd.read_excel(path_openfile_name,sheet_name=1)
+            RG = ReportGeneraler(input_file_path=path_openfile_name)
+            file_check = RG.input_file_check()
+            print(file_check)
+            if file_check is not True:
+                self.ExcelFilesState.setText('{:<40}'.format(file_check))
+                print(1)
+                return
+            else:
+                input_table = RG.file_data_adj()
+                self.ExcelFilesState.setText('{:<40}'.format('成功打开文件'))
+
+
         #print(input_table)
             input_table_rows = input_table.shape[0]
             input_table_colunms = input_table.shape[1]
